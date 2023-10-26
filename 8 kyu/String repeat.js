d1195cf271e42b513f573ae87ef2b6b8bb7e95cc
Longin89 <ivan.zhivoglyadov89@gmail.com>
2023-10-26 21:08:00 +0300

    added some completed tasks

---
+++ 8 kyu/String repeat.js
@@ -0,0 +1,23 @@
+/*
+
+	Description
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+
+6, "I"     -> "IIIIII"
+
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+*/
+
+//Solution
+
+function repeatStr (n, s) {
+    let res = '';
+  for(let i = 0; i < n; i++) {
+      res +=s;
+  }
+    return res;
+}
